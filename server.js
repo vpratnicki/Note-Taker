ef3142cc4353c3baaefd84eb50beb2de9fa7298d
vpratnicki <vanessa@pratnicki.net>
2022-10-03 15:41:31 -0400

    Add Heroku

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const { notes } = require('./db/db.json');
 
 // add the route
 app.get('/api/db', (req,res) => {
-    res.send('Hello!');
+    let results = notes;
+    console.log(req.query)
+    res.json(results);
 });
 app.listen(3002, () => {
     console.log(`API server now on port 3002!`);
