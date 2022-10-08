63d8a1fea30e12c71abf2c926b4d6d09336fd734
vpratnicki <vanessa@pratnicki.net>
2022-10-08 16:30:47 -0400

    Chore: users can post data to db.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 
@@ -28,6 +30,16 @@ function findById(id, notesArray) {
     const result = notesArray.filter(note => note.id === id)[0];
     return result;
   }
+  
+  function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    return note;
+  }
 
 // add the route
 app.get('/api/db', (req,res) => {
@@ -48,9 +60,13 @@ app.get('/api/db/:id', (req, res) => {
 
 // defined a route that listens for POST requests
 app.post('/api/db', (req, res) => {
-    // req.body is where our incoming content will be
-    console.log(req.body);
-    res.json(req.body);
+  // set id based on what the next index of the array will be
+  req.body.id = notes.length.toString();    
+
+  // add note to json file and notes array in this function
+  const note = createNewNote(req.body, notes);
+
+  res.json(note);
   });
 
 app.listen(PORT, () => {
