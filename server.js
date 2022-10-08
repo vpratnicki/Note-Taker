b8d68375f82336149cce30262d03005aeb5f6b0d
vpratnicki <vanessa@pratnicki.net>
2022-10-08 16:50:58 -0400

    Feature: Allow user creation of data - complete

--- server.js
+++ server.js
@@ -27,19 +27,29 @@ function filterByQuery(query, notesArray) {
 
 // takes in the id and array of notes & returns a single note object
 function findById(id, notesArray) {
-    const result = notesArray.filter(note => note.id === id)[0];
-    return result;
-  }
+   const result = notesArray.filter(note => note.id === id)[0];
+   return result;
+}
   
-  function createNewNote(body, notesArray) {
-    const note = body;
-    notesArray.push(note);
-    fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
-        JSON.stringify({ notes: notesArray }, null, 2)
-    );
-    return note;
-  }
+function createNewNote(body, notesArray) {
+   const note = body;
+   notesArray.push(note);
+   fs.writeFileSync(
+       path.join(__dirname, './db/db.json'),
+       JSON.stringify({ notes: notesArray }, null, 2)
+   );
+   return note;
+}
+
+function validateNote(note) {
+    if (!note.title || typeof note.title !== 'string') {
+        return false;
+    }
+    if (!note.text || typeof note.text != 'string') {
+        return false;
+    }
+    return true;
+}
 
 // add the route
 app.get('/api/db', (req,res) => {
@@ -63,10 +73,14 @@ app.post('/api/db', (req, res) => {
   // set id based on what the next index of the array will be
   req.body.id = notes.length.toString();    
 
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateNote(req.body)) {
+    res.status(400).send('The note is not properly formatted.');
+  } else {
   // add note to json file and notes array in this function
   const note = createNewNote(req.body, notes);
-
   res.json(note);
+  }
   });
 
 app.listen(PORT, () => {
