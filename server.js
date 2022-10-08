67dcc855d309af4e5674469bf22bdc2a5b4d4e14
vpratnicki <vanessa@pratnicki.net>
2022-10-08 15:55:16 -0400

    added filter functionality

--- server.js
+++ server.js
@@ -2,19 +2,57 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+//parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+//parse incoming json data
+app.use(express.json());
+
 // require the data
 const { notes } = require('./db/db.json');
 
+// filter functionality
+function filterByQuery(query, notesArray) {
+    let filteredResults = notesArray;
+    if (query.title) {
+        filteredResults = filteredResults.filter(note => note.title === query.title);
+    } 
+    if (query.text) {
+        filteredResults = filteredResults.filter(note => note.text === query.text)
+    }
+    return filteredResults;
+}
+
+// takes in the id and array of notes & returns a single note object
+function findById(id, notesArray) {
+    const result = notesArray.filter(note => note.id === id)[0];
+    return result;
+  }
 
 // add the route
 app.get('/api/db', (req,res) => {
-    let result = notes;
+    let results = notes;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+        res.json(results);
+});
+app.get('/api/db/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
     if (result) {
         res.json(result);
     } else {
         res.send(404);
     }
-});
+  });
+
+// defined a route that listens for POST requests
+app.post('/api/db', (req, res) => {
+    // req.body is where our incoming content will be
+    console.log(req.body);
+    res.json(req.body);
+  });
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
